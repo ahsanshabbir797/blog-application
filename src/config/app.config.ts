039bf4e07084b160023bb92ee7c98a608fc7c0b4
ahsanshabbir797 <ahsanshabbir797@gmail.com>
2026-02-27 19:04:26 +0500

    added app configuration and environment variables validation

---
+++ src/config/app.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('appConfig', () => ({
+  development: process.env.NODE_ENV || 'production',
+}));
+
+// export const appConfig = () => ({
+//   development: process.env.NODE_ENV || 'production',
+//   database: {
+//     host: process.env.DATABASE_HOST || 'localhost',
+//     port: parseInt(process.env.DATABASE_PORT ?? '5432', 10) || 5432,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     name: process.env.DATABASE_NAME,
+//     synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
+//     autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
+//   },
+// });
